b49f6e61b4f825e721fd8854f7e2205ab0604c25
Pedro Soler <pool@gft.com>
2023-03-30 11:22:11 +0000

    updated sql scripts

--- src/main/resources/sql/1_cart.sql
+++ src/main/resources/sql/1_cart.sql
@@ -2,7 +2,6 @@ insert into
 	CART (
 		id,
 		to_user,
-		to_tax_country,
 		created_at,
 		updated_at,
 		status
@@ -10,7 +9,6 @@ insert into
 values
 	(
 		'17d95cad-c198-4c06-812d-2cd2f345685f',
-		1,
 		1,
 		'1970-02-02 22:22:22',
 		'1970-02-02 22:22:22',
@@ -21,7 +19,6 @@ INSERT INTO
 	CART (
 		id,
 		to_user,
-		to_tax_country,
 		created_at,
 		updated_at,
 		status
@@ -30,7 +27,6 @@ VALUES
 	(
 		'9f99c3d3-1c8a-4e11-81e5-7c76e32c8d7f',
 		123,
-		4,
 		'2023-03-29 10:30:00',
 		'2023-03-29 10:31:00',
 		'DRAFT'
@@ -40,7 +36,6 @@ INSERT INTO
 	CART (
 		id,
 		to_user,
-		to_tax_country,
 		created_at,
 		updated_at,
 		status
@@ -49,7 +44,6 @@ VALUES
 	(
 		'73fbba50-6a2d-4218-a6a8-6fbd1d7b98c5',
 		456,
-		2,
 		'2023-03-28 18:45:00',
 		'2023-03-28 18:50:00',
 		'SUBMITTED'
@@ -59,7 +53,6 @@ INSERT INTO
 	CART (
 		id,
 		to_user,
-		to_tax_country,
 		created_at,
 		updated_at,
 		status
@@ -68,7 +61,6 @@ VALUES
 	(
 		'afa4bc90-2efd-46ad-ba72-61219ac9b8f5',
 		156,
-		3,
 		'2023-03-28 18:45:00',
 		'2023-03-28 18:50:00',
 		'DRAFT'
