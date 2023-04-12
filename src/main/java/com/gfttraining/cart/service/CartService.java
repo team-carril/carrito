66fa6096452151a94703e3cbda27dbf069c5b63b
Bryan <bnhn@gft.com>
2023-04-12 09:47:49 +0000

    Clear code getByUserId

--- src/main/java/com/gfttraining/cart/service/CartService.java
+++ src/main/java/com/gfttraining/cart/service/CartService.java
@@ -1,7 +1,6 @@
 package com.gfttraining.cart.service;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
