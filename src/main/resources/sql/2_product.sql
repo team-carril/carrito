10494b2eb292c2469a2cd85232beaab66cae4f70
Pedro Soler <pool@gft.com>
2023-04-18 08:37:41 +0000

    sql matches catalog

--- src/main/resources/sql/2_product.sql
+++ src/main/resources/sql/2_product.sql
@@ -1,15 +1,15 @@
-insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (1, '0aa4e49a-dbba-4a9e-a162-c3a9dc350f7f', 'Wine - Chablis 2003 Champs', 'Garden', 70, 6);
-insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (2, '0aa4e49a-dbba-4a9e-a162-c3a9dc350f7f', 'Beef - Ground Medium', 'Kids', 16, 8);
-insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (3, '0aa4e49a-dbba-4a9e-a162-c3a9dc350f7f', 'Crush - Orange, 355ml', 'Industrial', 6, 10);
-insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (4, '0aa4e49a-dbba-4a9e-a162-c3a9dc350f7f', 'Nori Sea Weed', 'Kids', 6, 12);
+insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (1, '0aa4e49a-dbba-4a9e-a162-c3a9dc350f7f', 'Ara ararauna', 'lorem vitae mattis nibh ligula nec sem duis', 1549, 1);
+insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (2, '0aa4e49a-dbba-4a9e-a162-c3a9dc350f7f', 'Hyanea brunnea', 'ultrices posuere cubilia ',  2366, 1);
+insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (3, '0aa4e49a-dbba-4a9e-a162-c3a9dc350f7f', 'Corythornis cristata', 'aenean auctor gravida sem praesent id massa id nisl venenatis ', 655.1, 2);
+insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (4, '0aa4e49a-dbba-4a9e-a162-c3a9dc350f7f', 'Thalasseus maximus', 'magna at nunc', 2362, 2);
 
-insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (2, '6e7baf09-9877-4c26-a4fb-21e0e2572819', 'Beef - Ground Medium', 'Kids', 16, 2);
-insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (5, '6e7baf09-9877-4c26-a4fb-21e0e2572819', 'Oil - Avocado', 'Grocery', 26, 16);
-insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (4, '6e7baf09-9877-4c26-a4fb-21e0e2572819', 'Nori Sea Weed', 'Kids', 6, 12);
+insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (2, '6e7baf09-9877-4c26-a4fb-21e0e2572819', 'Hyanea brunnea', 'ultrices posuere cubilia', 2366, 2);
+insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (5, '6e7baf09-9877-4c26-a4fb-21e0e2572819', 'Papio ursinus', 'rutrum rutrum neque anean', 2320, 3);
+insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (4, '6e7baf09-9877-4c26-a4fb-21e0e2572819', 'Thalasseus maximus', 'manga at nunc', 2362, 3);
 
-insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (5, '7e2bb8f9-6bbc-4bc4-915f-f72cb21b035f', 'Oil - Avocado', 'Grocery', 26, 9);
-insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (6, '7e2bb8f9-6bbc-4bc4-915f-f72cb21b035f', 'Chocolate Eclairs', 'Baby', 7, 5);
-insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (41, '7e2bb8f9-6bbc-4bc4-915f-f72cb21b035f', 'Bag - Clear 7 Lb', 'Automotive', 9, 1);
+insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (5, '7e2bb8f9-6bbc-4bc4-915f-f72cb21b035f', 'Papio ursinus', 'rutrum rutrum neque anean', 2899.45, 6);
+insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (6, '7e2bb8f9-6bbc-4bc4-915f-f72cb21b035f', 'Varanus sp.', 'ullamcorper augue a suscipit nulla ellit ac', 2320, 2);
+insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (41, '7e2bb8f9-6bbc-4bc4-915f-f72cb21b035f', 'Ceratotherium simum', 'pede venenatis non sodales sed tincidunt eu', 1545, 9);
 
 insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (1, '78fca8d3-05f6-4f1a-859f-ddab850ceec3', 'Wine - Chablis 2003 Champs', 'Garden', 70, 2);
 insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (6, '78fca8d3-05f6-4f1a-859f-ddab850ceec3', 'Chocolate Eclairs', 'Baby', 7, 15);
@@ -17,20 +17,20 @@ insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) v
 insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (8, '78fca8d3-05f6-4f1a-859f-ddab850ceec3', 'Lambcasing', 'Games', 81, 9);
 insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (9, '78fca8d3-05f6-4f1a-859f-ddab850ceec3', 'Flower - Leather Leaf Fern', 'Grocery', 41, 5);
 
-insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (6,  '50d0ffdd-a7c3-4bce-aadd-3dac8732571f', 'Chocolate Eclairs', 'Baby', 7, 15);
-insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (16, '50d0ffdd-a7c3-4bce-aadd-3dac8732571f', 'Wine - Winzer Krems Gruner', 'Grocery', 7, 13);
-insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (93, '50d0ffdd-a7c3-4bce-aadd-3dac8732571f', 'Pepper - Cubanelle', 'Sports', 43, 12);
-insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (44, '50d0ffdd-a7c3-4bce-aadd-3dac8732571f', 'Beer - Alexander Kieths, Pale Ale', 'Outdoors', 83, 2);
+insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (6,  '50d0ffdd-a7c3-4bce-aadd-3dac8732571f', 'Varanus sp.', 'Babmcorper auge a suscipit nulla ellit ac', 2120, 2 );
+insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (16, '50d0ffdd-a7c3-4bce-aadd-3dac8732571f', 'Petauraus norfolcensis', 'velit nec nisi vulputate nonummy maecenas tincidunt lacus', 1152, 7);
+insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (93, '50d0ffdd-a7c3-4bce-aadd-3dac8732571f', 'Eolophus roseicapiullus', 'leo pellentesque ultrices mattis odio donec vitae nisi nam', 1142, 43);
+insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (44, '50d0ffdd-a7c3-4bce-aadd-3dac8732571f', 'Struthio camelus', 'ut nunc vestibulum', 83, 2334);
 
-insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (7, '0f6f7a4f-1097-4624-8973-ae6966728647', 'Wine - Chenin Blanc K.w.v.', 'Games', 89, 11);
-insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (41, '0f6f7a4f-1097-4624-8973-ae6966728647', 'Bag - Clear 7 Lb', 'Automotive', 9, 3);
-insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (62, '0f6f7a4f-1097-4624-8973-ae6966728647', 'Kiwi', 'Sports', 33, 9);
+insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (7, '0f6f7a4f-1097-4624-8973-ae6966728647', 'Pseudoleistes virescens', 'nunc rhoncus dui vel sem sed sagittis', 1686, 89);
+insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (41, '0f6f7a4f-1097-4624-8973-ae6966728647', 'Ceratothereium simum', 'pede venenantis non sodales sed tincdunt eu', 1545, 9);
+insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (62, '0f6f7a4f-1097-4624-8973-ae6966728647', 'Theropithecus gelada', 'non sodales sed tincidunt eu', 2296, 33);
 
 insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (9, '3a9018dc-1506-439a-b0d4-1451fd5d8dae', 'Flower - Leather Leaf Fern', 'Grocery', 41, 5);
 insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (7,'3a9018dc-1506-439a-b0d4-1451fd5d8dae', 'Wine - Chenin Blanc K.w.v.', 'Games', 9, 11);
 insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (2, '3a9018dc-1506-439a-b0d4-1451fd5d8dae', 'Beef - Ground Medium', 'Kids', 16, 20);
 
-insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (10, '18df6192-4b31-4951-9756-4a277acba7ae', 'Glaze - Clear', 'Movies', 97, 4);
+insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (10, '18df6192-4b31-4951-9756-4a277acba7ae', 'Alligator mississipiensis', 'odio porttitor id consequat ut nulla', 383.1, 11);
 
 insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (10, 'afa3350e-51da-468a-b288-1c41e317eb73', 'Glaze - Clear', 'Movies', 47, 2);
 insert into PRODUCT (catalog_id , to_cart, name, description, price, quantity) values (62, 'afa3350e-51da-468a-b288-1c41e317eb73', 'Kiwi', 'Sports', 35, 2);
